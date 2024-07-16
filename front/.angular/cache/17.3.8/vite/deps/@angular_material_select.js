248ad1eaf8539d45bc27785e30386efc6dc4f943
Stanislav Kirisenko <s.kirisenko@dex-it.ru>
2024-07-16 10:33:17 +0300

    add license and readme

---
+++ front/.angular/cache/17.3.8/vite/deps/@angular_material_select.js
@@ -0,0 +1,54 @@
+import {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  matSelectAnimations
+} from "./chunk-N5A64EKD.js";
+import {
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix
+} from "./chunk-QPZM4YXZ.js";
+import "./chunk-UZLQAKCI.js";
+import "./chunk-2I4OVMKI.js";
+import "./chunk-O5WT2R46.js";
+import "./chunk-XQMYJXJP.js";
+import "./chunk-72MTWRGL.js";
+import {
+  MatOptgroup,
+  MatOption
+} from "./chunk-ND6WLMTI.js";
+import "./chunk-4MOHW5LC.js";
+import "./chunk-ECI3W7TP.js";
+import "./chunk-4J25ECOH.js";
+import "./chunk-J4B6MK7R.js";
+export {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatOptgroup,
+  MatOption,
+  MatPrefix,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  MatSuffix,
+  matSelectAnimations
+};
+//# sourceMappingURL=@angular_material_select.js.map
